9da110792c60a975ec9cec0474fdf8b78de03f2f
King-elu <131325609+King-elu@users.noreply.github.com>
2023-06-04 18:15:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -54,15 +54,7 @@ document.querySelector("#btn").addEventListener("click",()=>{
     weather.search()
     document.querySelector("#searchbar").value = ""
 })
-navigator.geolocation.getCurrentPosition(position =>{
-        const { latitude, longitude}=position.coords;
-        const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
-        fetch(url).then(res =>res.json()).then(data =>{
-            const country = data.address.coutry
-
-
-})})
-weather.fetchWeather(country)
+weather.fetchWeather("kenya")
 
 
 
